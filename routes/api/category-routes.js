44fa37f231fe518657a876ea82cad4ed018de4ae
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-10-30 13:16:51 -0400

    Minor updates made to routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,24 +49,24 @@ router.put("/:id", (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
     where: {
-      id: req.params.id
+      id: req.params.id,
     }
-    .then((dbCategoryData) => res.json(dbCategoryData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-  })
+      .then((dbCategoryData) => res.json(dbCategoryData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      }),
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
   Category.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-  .then((dbCategoryData) => res.json(dbCategoryData))
+    .then((dbCategoryData) => res.json(dbCategoryData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
